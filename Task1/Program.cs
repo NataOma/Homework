14a2f30ad1cb71021cb056963896d96cfe29b55d
Natalia <chekiska.11@mail.ru>
2024-05-26 20:19:57 +0500

    Добавлено Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Вычислить факториал N!
+
+int rezult = 1;
+
+void FactorialN(int n, int x)
+{
+    if(x == n + 1)
+    {
+        Console.WriteLine($"Факториал числа N равен {rezult}");
+        return;
+    }
+    
+    rezult *= x;
+
+    FactorialN(n, x + 1);
+}
+
+Console.WriteLine("Введи значение числа N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+
+FactorialN(n, x);
