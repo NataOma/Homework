3d23e53bf312e6ebf5e7b26f290d6a4e15f84e8a
Natalia <chekiska.11@mail.ru>
2024-05-26 20:20:11 +0500

    Добавлено Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Вычислим факториал числа n
+
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        // Console.WriteLine($"Стоп при n={n}");
+        return 1;
+
+    }
+    // Console.WriteLine(n);
+    
+    int rez = (n * Fact(n - 1));
+    Console.WriteLine($"Возврат n= {n}, Fact={rez}");
+    return rez;
+}
+
+Console.Write(Fact(5));
+
+
+
+// int Factorial(int n)
+// {
+// 	if(n == 1) return 1;
+// 	return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(5)); // 120
+
+
+// int Fact(int n)
+// {
+//         if (n == 1 || n == 0)
+//     {
+//         Console.WriteLine($"Stop: {n}");
+//         return 1;
+//     }
+//     Console.WriteLine(n);
+//     return n * Fact(n - 1);
+// }
+
+// Console.WriteLine(Fact(5));
